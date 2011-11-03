6351149fbfaf0b7dedf8f4eb5e6f57d7d05f3da5
Martin Trigaux <me@mart-e.be>
2011-11-03 17:52:20 +0100

    correcting enter nonce

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python2
 import sys
 
+from smartutils import SmartUtils
+from smartcard.util import toHexString, toBytes
+
 def getNonce():
+    
     print("Enter a nonce or <Enter> for default value.\nThe nonce should be 8 bytes-long in hexadecimal\nDefault: 11 22 33 44 55 66 77 88.")
-    nonce = raw_input("Nonce: ")
-    if checkNonce(nonce):
-        return toBytes(nonce)
+    
+    
+    is_nonce = False
+    while True:
+        nonce = raw_input("Nonce: ")
+        if len(nonce) == 0:
+            # use default
+            return toBytes("11 22 33 44 55 66 77 88")
+        else:
+            if checkNonce(nonce):
+                return toBytes(nonce)
 
 def checkNonce(nonce):
-    if len(nonce) == 0:
-        n = toBytes("11 22 33 44 55 66 77 88")
-    else:
-        try:
-            n = toBytes(nonce)
-            if len(n)==8:
-                print("nonce ok")
-            else:
-                raise Exception
-        except:
-            print(nonce+" is not valid, selecting default value")
-            n = toBytes("11 22 33 44 55 66 77 88")
+    try:
+        n = toBytes(nonce)
+        if len(n)==8:
+            # is hex and 8 bytes
+            return True
+    except:
+        pass
+    
+    print(nonce+" is not valid")   
+    return False
 
 
 if __name__ == "__main__":
@@ -27,4 +37,4 @@ if __name__ == "__main__":
     s.poll()
 
     nonce = getNonce()
-    s.auth(n)
+    s.auth(nonce)
