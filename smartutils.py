cf955d25580f68c5acddd9154ab9674e640c479b
Martin Trigaux <me@mart-e.be>
2011-11-29 09:52:36 +0100

    with status msg

--- smartutils.py
+++ smartutils.py
@@ -182,7 +182,7 @@ class SmartUtils:
                 data, sw1, sw2 = self.getAnswer (sw2)
                 dfdata, dfsw1, dfsw2 = errors.evaluateResponse (data)
                 return errors.isOpOk (dfsw1, dfsw2)
-        self._createStdDataFile('Creating standard data file #%x' % fileNo, _createStdDataFile)
+        self._withStatusMsg('Creating standard data file #%x' % fileNo, _createStdDataFile)
 
 
     def deleteStdDataFile(self, fileNo = 0x01):
@@ -197,7 +197,7 @@ class SmartUtils:
                 data, sw1, sw2 = self.getAnswer (sw2)
                 dfdata, dfsw1, dfsw2 = errors.evaluateResponse (data)
                 return errors.isOpOk (dfsw1, dfsw2)
-        self._deleteStdDataFile('Deleting standard data file #%x' % fileNo, _createStdDataFile)
+        self._withStatusMsg('Deleting standard data file #%x' % fileNo, _createStdDataFile)
 
 
     def readStdDataFile(self, fileNo = 0x01, offset = [0x00, 0x00, 0x00], readingLength = [0x00, 0x00, 0x00]):
